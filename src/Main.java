616b0a78f6983c531e00e8fe508a723967645979
Roni Hell <prosto.lebed2014@yandex.ru>
2022-02-17 17:19:14 +0300

    Second variant

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int client1 = 100;
-        int client2 = 100;
-        int added1 = 100;
-        int added2 = 1100;
-        int bonus1 = 0;
-        int bonus2 = 0;
+        int client_bill = 100;
+        // int plus = 100;
+        int plus = 1100;
+        int bonus = 0;
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
         // суммы пополнения и тп
-        client1 = client1 + added1;
-        if (added1 > 1000) {
-            bonus1 = added1 / 100;
+        client_bill = client_bill + plus;
+        if (plus > 1000) {
+            bonus = plus / 100;
         }
-        client2 = client2 + added2;
-        if (added2 > 1000) {
-            bonus2 = added2 / 100;
-        }
-        System.out.println(bonus1);
-        System.out.println(client1 + bonus1);
-
-        System.out.println(bonus2);
-        System.out.println(client2 + bonus2);
-
+        System.out.println(client_bill + bonus);
         // Условным оператором проверяете превысила ли
         // сумма пополнения порог и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
     }
-}
+
+    }
