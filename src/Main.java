7f22198ef0242c3ec3af1d037a3b2c7db9171873
Roni Hell <prosto.lebed2014@yandex.ru>
2022-03-10 23:12:22 +0300

    Mistakes were fixed

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int client_bill = 100;
+        int clientBill = 100;
         // int plus = 100;
         int plus = 1100;
         int bonus = 0;
+
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
         // суммы пополнения и тп
-        client_bill = client_bill + plus;
+
+        clientBill = clientBill + plus;
         if (plus > 1000) {
             bonus = plus / 100;
         }
-        System.out.println(client_bill + bonus);
+
+        System.out.println(clientBill + bonus);
+        System.out.println(bonus);
         // Условным оператором проверяете превысила ли
         // сумма пополнения порог и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
